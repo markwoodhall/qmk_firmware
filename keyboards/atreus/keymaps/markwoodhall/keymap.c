e3d5e62286c8ca7d43913b173bf90e745b7fc892
Mark Woodhall <mark.woodhall@gmail.com>
2019-05-07 10:07:30 +0100

    Permissive hold and paren changes

--- keyboards/atreus/keymaps/markwoodhall/keymap.c
+++ keyboards/atreus/keymaps/markwoodhall/keymap.c
@@ -13,22 +13,23 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QW] = LAYOUT( /* Qwerty */
-  KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
-  KC_A,    KC_S,    KC_D,     KC_F,    KC_G,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
-  KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,                                           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-  KC_LGUI, KC_DELT, KC_LALT,  KC_TAB,  ALT_T(KC_ENT),  CTL_T(KC_BSPC),  SFT_T(KC_SPC), KC_ESC,  MO(_RS), KC_MINS, KC_QUOT, KC_GRV  ),
+  KC_Q,    KC_W,    KC_E,    KC_R,            KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
+  KC_A,    KC_S,    KC_D,    KC_F,            KC_G,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
+  KC_Z,    KC_X,    KC_C,    KC_V,            KC_B,                                           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
+  KC_LGUI, KC_DELT, KC_LALT, LT(_RS, KC_TAB), ALT_T(KC_ENT),  CTL_T(KC_BSPC),  SFT_T(KC_SPC), KC_ESC,  MO(_RS), KC_MINS, KC_QUOT, KC_GRV  ),
 
 [_RS] = LAYOUT( /* [> RAISE <] */
-  KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                                 KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR ,
-  KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                                  KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS ,
-  KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                                 KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS ,
-  TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, CTL_T(KC_BSPC),  SFT_T(KC_SPC), KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
+  KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                           KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR ,
+  KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                            KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS ,
+  KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_AMPR,                           KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS ,
+  TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_DOUBLE_QUOTE, KC_LPRN, KC_RPRN, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
 
 [_LW] = LAYOUT( /* [> LOWER <] */
-  KC_INS,  KC_HOME,    KC_MS_UP,   KC_END,      KC_PGUP,                         KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-  KC_DELT, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_DOWN,                         KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-  KC_NO,   KC_VOLU,    KC_NO,      KC_NO,       RESET,                           KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-  KC_NO,   KC_VOLD,    KC_LGUI,    KC_LSFT,     KC_BSPC, KC_MS_BTN1, KC_MS_BTN2, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS )
+  KC_NO, KC_NO,      KC_MS_UP,   KC_NO,       KC_NO,                         KC_NO, KC_F7,   KC_F8, KC_F9, KC_F10  ,
+  KC_NO, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_NO,                         KC_NO, KC_F4,   KC_F5, KC_F6, KC_F11  ,
+  KC_NO, KC_NO,      KC_NO,      KC_NO,       RESET,                         KC_NO, KC_F1,   KC_F2, KC_F3, KC_F12  ,
+  KC_NO, KC_NO,      KC_NO,      KC_NO,       KC_NO, KC_MS_BTN1, KC_MS_BTN2, KC_NO, TO(_QW), KC_NO, KC_NO, KC_NO )
+
 };
 
 const uint16_t PROGMEM fn_actions[] = {

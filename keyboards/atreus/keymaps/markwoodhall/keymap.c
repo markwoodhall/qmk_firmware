635f2ea66a0c1238f57dfe809cedd70241359594
Mark Woodhall <mark.woodhall@gmail.com>
2020-08-13 13:27:21 +0100

    Adjust layout

--- keyboards/atreus/keymaps/markwoodhall/keymap.c
+++ keyboards/atreus/keymaps/markwoodhall/keymap.c
@@ -10,25 +10,32 @@
 #define _QW 0
 #define _RS 1
 #define _LW 2
+#define _LW2 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QW] = LAYOUT( /* Qwerty */
   KC_Q,    KC_W,    KC_E,    KC_R,            KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
   KC_A,    KC_S,    KC_D,    KC_F,            KC_G,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
   KC_Z,    KC_X,    KC_C,    KC_V,            KC_B,                                           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-  KC_LGUI, KC_DELT, KC_LALT, LT(_RS, KC_TAB), ALT_T(KC_ENT),  CTL_T(KC_BSPC),  SFT_T(KC_SPC), KC_ESC,  MO(_RS), KC_MINS, KC_QUOT, KC_GRV  ),
+  KC_NO, KC_DELT, KC_LALT, LT(_RS, KC_TAB), ALT_T(KC_ENT),  CTL_T(KC_BSPC),  SFT_T(KC_SPC), KC_ESC,  MO(_RS), KC_MINS, KC_QUOT, KC_GRV  ),
 
 [_RS] = LAYOUT( /* [> RAISE <] */
   KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                           KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR ,
   KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                            KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS ,
   KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_AMPR,                           KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS ,
-  TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_DOUBLE_QUOTE, KC_LPRN, KC_RPRN, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
+  KC_NO, KC_INS,  KC_LGUI, KC_LSFT, KC_DOUBLE_QUOTE, KC_LPRN, KC_RPRN, TO(_LW),  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
 
 [_LW] = LAYOUT( /* [> LOWER <] */
-  KC_NO, KC_NO,      KC_MS_UP,   KC_NO,       KC_NO,                         KC_NO, KC_F7,   KC_F8, KC_F9, KC_F10  ,
-  KC_NO, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_NO,                         KC_NO, KC_F4,   KC_F5, KC_F6, KC_F11  ,
-  KC_NO, KC_NO,      KC_NO,      KC_NO,       RESET,                         KC_NO, KC_F1,   KC_F2, KC_F3, KC_F12  ,
-  KC_NO, KC_NO,      KC_NO,      KC_NO,       KC_NO, KC_MS_BTN1, KC_MS_BTN2, KC_NO, TO(_QW), KC_NO, KC_NO, KC_NO )
+  KC_Q,    KC_W,    KC_E,    KC_R,            KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
+  KC_A,    KC_S,    KC_D,    KC_F,            KC_G,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
+  KC_Z,    KC_X,    KC_C,    KC_V,            KC_B,                                           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
+  KC_NO, KC_DELT, KC_LALT, LT(_LW2, KC_TAB), ALT_T(KC_ENT),  CTL_T(KC_BSPC),  SFT_T(KC_SPC), KC_ESC,  TO(_QW), KC_MINS, KC_QUOT, KC_GRV  ),
+
+[_LW2] = LAYOUT( /* [> LOWER <] */
+  KC_1,    KC_2,    KC_3,    KC_4,            KC_5,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
+  KC_6,    KC_7,    KC_8,    KC_9,            KC_0,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
+  KC_Z,    KC_X,    KC_C,    KC_V,            KC_B,                                           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
+  KC_NO, KC_DELT, KC_LALT,   KC_NO, ALT_T(KC_ENT),  CTL_T(KC_BSPC),  SFT_T(KC_SPC), KC_ESC,  TO(_QW), KC_MINS, KC_QUOT, KC_GRV  )
 
 };
 
